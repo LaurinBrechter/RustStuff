f86fa791b42221fde2dbc3766ce41a8b14f9753f
laurinbrechter <brechterlaurin@gmail.com>
2024-06-30 21:42:57 +0200

    init rastrigin-pso project

--- optim-rust/rastrigin-pso/src/main.rs
+++ optim-rust/rastrigin-pso/src/main.rs
@@ -1,3 +1,7 @@
+use std::{collections::btree_set::Intersection, process::exit};
+
+use rand::{rngs::ThreadRng, Rng};
+
 
 fn rastrigin(x: Vec<f64>) -> f64{
 
@@ -8,23 +12,81 @@ fn rastrigin(x: Vec<f64>) -> f64{
     return (x.len() as f64)*A + l.sum::<f64>()
 }
 
+struct Swarm {
+    global_best: Vec<f32>,
+    inertia: f32,
+    c1: f32,
+    c2: f32,
+}
+
+
+impl Vec<T> {
+    fn add()
+}
+
+
+
+fn add_vectors(a: &Vec<f32>, b: &Vec<f32>) -> Vec<f32> {
+    a.iter().zip(b.iter()).map(|(x,y)| x+y).collect()
+}
+
+fn scalar_multiply(a: &Vec<f32>, b:f32) -> Vec<f32> {
+    a.iter().map(|x| x*b).collect()
+}
+
+
+struct Particle<'a> {
+    swarm: &'a Swarm,
+    position: Vec<f32>,
+    speed: Vec<f32>,
+    local_best: Vec<f32>,
+}
+
+impl<'a> Particle<'a> {
+    fn new_position(&self)  -> Vec<f32> {
+        return add_vectors(&self.speed, &self.position)
+    }
+    fn new_speed(&self, rng: & mut ThreadRng) -> Vec<f32> {
+
+        let inertia = self.swarm.inertia;
+        let r1 = rng.gen::<f32>();
+
+        let r2 = rng.gen::<f32>();
+
+        let negated_pos: Vec<f32> = self.local_best.iter().map(|x| -x).collect();
+
+
+        return scalar_multiply(&self.speed, inertia) +
+
+
+
+    }
+}
 
 
 fn main() {
     println!("Hello, world!");
+    
 
-    let x = vec![1.2, 3.0];
-    let min_x = vec![0.0;10000000];
+    let mut rng = rand::thread_rng();
 
-    use std::time::Instant;
-    let now = Instant::now();
+    println!("{}", rng.gen::<f32>());
+    
 
-    let y = rastrigin(min_x);
+    exit(0);
 
-    let elapsed = now.elapsed();
+    // let x = vec![];
+    // let min_x = vec![0.0;10000000];
+
+    // use std::time::Instant;
+    // let now = Instant::now();
+
+    // let y = rastrigin(min_x);
+
+    // let elapsed = now.elapsed();
 
 
-    println!("Result: {}, Elapsed: {:?}", y, elapsed)
+    // println!("Result: {}, Elapsed: {:?}", y, elapsed)
 
 
 }
